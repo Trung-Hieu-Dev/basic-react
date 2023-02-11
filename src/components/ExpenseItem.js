d17d2567e298ff1b04423247d78823146ce0d208
Trung Hieu <hieu@ltv.dev>
2023-02-11 11:29:26 +0700

    Date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem (props) {
-    const expenseDate = new Date(2023, 2, 11);
+    const day = props.date.toLocaleString('en-US', {month: '2-digit'});
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
 
     return (
         <div className='expense-item '>
-            <div>{ expenseDate.toISOString() }</div>
+            <div>
+                <div>{ month }</div>
+                <div>{ year }</div>
+                <div>{ day }</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{ props.title }</h2>
                 <div className='expense-item__price'>${ props.amount }</div>
